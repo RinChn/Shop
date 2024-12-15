b112235310ccde45b146bb66350d20fa2728c801
RinChn <morik.arina@yandex.ru>
2024-12-15 21:08:12 +0300

    Adding Lombok annotations

--- src/main/java/entities/Product.java
+++ src/main/java/entities/Product.java
@@ -1,31 +1,40 @@
 package entities;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.sql.Timestamp;
 import java.util.UUID;
 
 @Entity
 @Table(name = "product")
+@Getter
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
             @Column(name = "id", nullable = false)
     UUID id;
     @Column(name = "name", nullable = false, length = 255)
+            @Setter
     String name;
     @Column(name = "description")
+            @Setter
     String description;
     @Column(name = "categories")
+            @Setter
     Category categories;
     @Column(name = "price", nullable = false)
+            @Setter
     float price;
     @Column(name = "quantity")
+            @Setter
     int quantity = 0;
     @Column(name = "article_number", nullable = false)
     long articleNumber;
     @Column(name = "date_last_changes_quantity")
+            @Setter
     Timestamp dateOfLastChangesQuantity = new Timestamp(System.currentTimeMillis());
     @Column(name = "date_creation")
-    Timestamp dateOfCreation = new Timestamp(System.currentTimeMillis());
+    final Timestamp dateOfCreation = new Timestamp(System.currentTimeMillis());
 }
