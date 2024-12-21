c39c8807d014ee956abef831ed9d3c094fc866f1
RinChn <morik.arina@yandex.ru>
2024-12-21 23:11:33 +0300

    Filling in the response

---
+++ src/main/java/dto/ProductResponse.java
@@ -0,0 +1,23 @@
+package dto;
+
+import entities.Category;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ProductResponse {
+    private UUID articleNumber;
+    private String name;
+    private String description;
+    private Category categories;
+    private Float price;
+    private Integer quantity;
+    private Timestamp dateOfLastChangesQuantity;
+    private Timestamp dateOfCreation;
+}
