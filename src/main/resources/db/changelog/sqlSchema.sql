603e1d587bc00b6ffa9db206d88a2f964a43f474
RinChn <morik.arina@yandex.ru>
2024-12-22 18:41:06 +0300

    Renaming the sql article field to a name with an underscore

--- src/main/resources/db/changelog/sqlSchema.sql
+++ src/main/resources/db/changelog/sqlSchema.sql
@@ -1,9 +1,9 @@
 --liquibase formatted sql
---changeset morik:3
+--changeset morik:4
 
 CREATE TABLE product
 (
-    articleNumber UUID NOT NULL default gen_random_uuid(),
+    article_number UUID NOT NULL default gen_random_uuid(),
     name VARCHAR(255) NOT NULL,
     description TEXT,
     categories VARCHAR(255),
@@ -11,5 +11,5 @@ CREATE TABLE product
     quantity INTEGER default 0,
     date_last_changes_quantity TIMESTAMP default CURRENT_TIMESTAMP,
     date_creation TIMESTAMP default CURRENT_TIMESTAMP,
-    PRIMARY KEY (articleNumber)
+    PRIMARY KEY (article_number)
 );
