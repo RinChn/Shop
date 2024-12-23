f19097d28d36e802f5a12dffdadfc53eba58d528
RinChn <morik.arina@yandex.ru>
2024-12-24 00:14:55 +0300

    Replacing IDENTITY with SERIAL in an SQL script

--- src/main/resources/db/changelog/001-create-product-table.sql
+++ src/main/resources/db/changelog/001-create-product-table.sql
@@ -4,7 +4,7 @@
 CREATE TABLE product
 (
     id UUID NOT NULL default gen_random_uuid(),
-    article INTEGER NOT NULL UNIQUE IDENTITY(1, 1),
+    article SERIAL NOT NULL UNIQUE,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     categories VARCHAR(255),
@@ -12,5 +12,5 @@ CREATE TABLE product
     quantity INTEGER default 0,
     date_last_changes_quantity TIMESTAMP default CURRENT_TIMESTAMP,
     date_creation TIMESTAMP default CURRENT_TIMESTAMP,
-    PRIMARY KEY (article_number)
+    PRIMARY KEY (id)
 );
