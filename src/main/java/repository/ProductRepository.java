f917d1ff5f272048426f73f2b2740776b473ad24
RinChn <morik.arina@yandex.ru>
2024-12-24 00:01:10 +0300

    Adding the findByArticle method to the repository

--- src/main/java/repository/ProductRepository.java
+++ src/main/java/repository/ProductRepository.java
@@ -2,8 +2,14 @@ package repository;
 
 import entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
 import java.util.UUID;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
+    @Query("SELECT p FROM Product p WHERE p.article=?")
+    Product findByArticle(String article);
 }
