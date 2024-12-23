d17e5797d56ed2064402c626dd9f665ba302c94d
RinChn <morik.arina@yandex.ru>
2024-12-23 22:36:49 +0300

    Changing prefixes in the controller

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -2,6 +2,7 @@ package controller;
 
 import dto.ProductRequest;
 import dto.ProductResponse;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,18 +11,14 @@ import service.ProductService;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product")
+@RequestMapping("/api/v1/product")
 @Slf4j
+@RequiredArgsConstructor
 public class ProductController {
 
     private final ProductService productService;
 
-    @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
-
-    @PostMapping("/add")
+    @PostMapping("")
     public String addProduct(@RequestBody ProductRequest productDto) {
         log.info("Product creation request from the customer {}", productDto);
         return productService.createProduct(productDto);
@@ -33,17 +30,17 @@ public class ProductController {
         return productService.getAllProducts();
     }
 
-    @DeleteMapping("/delete/{article}")
+    @DeleteMapping("/{article}")
     public String deleteProduct(@PathVariable String article) {
         log.info("Deleting product {} from the customer", article);
         return productService.deleteProduct(article);
     }
 
-    @PutMapping("/update/{article}/name/{newName}")
-    public String updateProduct(@PathVariable("article") String productUUID,
+    @PutMapping("/{article}/name/{newName}")
+    public String updateProduct(@PathVariable("article") String productArticle,
                                 @PathVariable("newName") String newName) {
-        log.info("Updating product {} from the customer", productUUID);
-        return productService.updateName(productUUID, newName);
+        log.info("Updating product {} from the customer", productArticle);
+        return productService.updateName(productArticle, newName);
     }
 
 }
