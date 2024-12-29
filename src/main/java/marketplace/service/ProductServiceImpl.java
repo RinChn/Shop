2a1aab4bebdc0e420bec366ffa9593c26ddfa43e
RinChn <morik.arina@yandex.ru>
2024-12-29 17:58:20 +0300

    Fixed conversion to List in the method of receiving all products in the service

--- src/main/java/marketplace/service/ProductServiceImpl.java
+++ src/main/java/marketplace/service/ProductServiceImpl.java
@@ -39,7 +39,7 @@ public class ProductServiceImpl implements ProductService {
         return productRepository.findAll()
                 .stream()
                 .map(product -> conversionService.convert(product, ProductResponse.class))
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Override
