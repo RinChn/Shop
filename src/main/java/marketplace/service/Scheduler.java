0bd58bfca8d72bb313466c8f99a619090a56d072
RinChn <morik.arina@yandex.ru>
2025-01-05 13:41:25 +0300

    Delete async

--- src/main/java/marketplace/service/Scheduler.java
+++ src/main/java/marketplace/service/Scheduler.java
@@ -5,15 +5,12 @@ import lombok.extern.slf4j.Slf4j;
 import marketplace.repository.ProductRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.transaction.annotation.Transactional;
 
 @Configuration
 @Slf4j
-@EnableAsync
 @EnableScheduling
 @ConditionalOnProperty(value = "scheduler.enabled", matchIfMissing = false)
 @RequiredArgsConstructor
@@ -23,7 +20,6 @@ public class Scheduler {
 
     @Scheduled(fixedRateString = "${scheduler.period}")
     @Transactional
-    @Async
     void priceIncrease() {
         productRepository.increasePrices();
         log.info("Price increased");
