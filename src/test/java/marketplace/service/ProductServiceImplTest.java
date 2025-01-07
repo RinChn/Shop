2f2acb780eaf492a3a3f2a38c9765842953370f2
RinChn <morik.arina@yandex.ru>
2025-01-07 16:00:11 +0300

    Static import Mockito.when

--- src/test/java/marketplace/service/ProductServiceImplTest.java
+++ src/test/java/marketplace/service/ProductServiceImplTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.data.domain.PageImpl;
@@ -20,6 +19,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.when;
 
 
 @ExtendWith(MockitoExtension.class)
@@ -33,11 +33,11 @@ public class ProductServiceImplTest {
 
     @Test
     void getAllProducts() {
-        Mockito.when(productRepository.findAll(PageRequest.of(0, 5)))
+        when(productRepository.findAll(PageRequest.of(0, 5)))
                 .thenReturn(new PageImpl<>(List.of(TestProduct.dress, TestProduct.ball)));
-        Mockito.when(conversionService.convert(TestProduct.dress, ProductResponse.class))
+        when(conversionService.convert(TestProduct.dress, ProductResponse.class))
                 .thenReturn(TestProduct.dressResponse);
-        Mockito.when(conversionService.convert(TestProduct.ball, ProductResponse.class))
+        when(conversionService.convert(TestProduct.ball, ProductResponse.class))
                 .thenReturn(TestProduct.ballResponse);
 
         assertEquals(List.of(TestProduct.dressResponse, TestProduct.ballResponse),
@@ -46,9 +46,9 @@ public class ProductServiceImplTest {
 
     @Test
     void getProduct_successfully() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.ofNullable(TestProduct.ball));
-        Mockito.when(conversionService.convert(TestProduct.ball, ProductResponse.class))
+        when(conversionService.convert(TestProduct.ball, ProductResponse.class))
                 .thenReturn(TestProduct.ballResponse);
         assertEquals(TestProduct.ballResponse,
                 productService.getProduct(TestProduct.ball.getArticle()));
@@ -56,7 +56,7 @@ public class ProductServiceImplTest {
 
     @Test
     void getProduct_notFound() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.empty());
         assertThrows(ApplicationException.class, () ->
             productService.getProduct(TestProduct.ball.getArticle())
@@ -65,11 +65,11 @@ public class ProductServiceImplTest {
 
     @Test
     void createProduct_successfully() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.empty());
-        Mockito.when(conversionService.convert(TestProduct.ball, ProductResponse.class))
+        when(conversionService.convert(TestProduct.ball, ProductResponse.class))
                 .thenReturn(TestProduct.ballResponse);
-        Mockito.when(conversionService.convert(TestProduct.ballRequestCreate, Product.class))
+        when(conversionService.convert(TestProduct.ballRequestCreate, Product.class))
                 .thenReturn(TestProduct.ball);
         assertEquals(TestProduct.ballResponse,
                 productService.createProduct(TestProduct.ballRequestCreate));
@@ -77,9 +77,9 @@ public class ProductServiceImplTest {
 
     @Test
     void createProduct_duplicate() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.ofNullable(TestProduct.ball));
-        Mockito.when(conversionService.convert(TestProduct.ballRequestCreate, Product.class))
+        when(conversionService.convert(TestProduct.ballRequestCreate, Product.class))
                 .thenReturn(TestProduct.ball);
         assertThrows(ApplicationException.class, () ->
                 productService.createProduct(TestProduct.ballRequestCreate));
@@ -87,7 +87,7 @@ public class ProductServiceImplTest {
 
     @Test
     void deleteProduct_successfully() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.ofNullable(TestProduct.ball));
         assertEquals(TestProduct.ball.getId(),
                 productService.deleteProduct(TestProduct.ball.getArticle()));
@@ -95,7 +95,7 @@ public class ProductServiceImplTest {
 
     @Test
     void deleteProduct_notFound() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.empty());
         assertThrows(ApplicationException.class, () ->
                 productService.deleteProduct(TestProduct.ball.getArticle()));
@@ -103,9 +103,9 @@ public class ProductServiceImplTest {
 
     @Test
     void updateProduct_successfully() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.ofNullable(TestProduct.ball));
-        Mockito.when(conversionService.convert(TestProduct.ball, ProductResponse.class))
+        when(conversionService.convert(TestProduct.ball, ProductResponse.class))
                 .thenReturn(TestProduct.ballResponseAfterUpd);
         assertEquals(TestProduct.ballResponseAfterUpd,
                 productService.updateProduct(TestProduct.ballRequestUpdate, TestProduct.ball.getArticle()));
@@ -113,7 +113,7 @@ public class ProductServiceImplTest {
 
     @Test
     void updateProduct_notFound() {
-        Mockito.when(productRepository.findByArticle(TestProduct.ball.getArticle()))
+        when(productRepository.findByArticle(TestProduct.ball.getArticle()))
                 .thenReturn(Optional.empty());
         assertThrows(ApplicationException.class, () ->
                 productService.updateProduct(TestProduct.ballRequestUpdate, TestProduct.ball.getArticle()));
