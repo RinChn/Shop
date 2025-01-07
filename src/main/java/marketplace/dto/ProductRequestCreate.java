08b79ced501b415b9a994aa6dad230eefafaa2e2
RinChn <morik.arina@yandex.ru>
2025-01-07 22:32:29 +0300

    Replacing NotNull with NotBlank for the name

--- src/main/java/marketplace/dto/ProductRequestCreate.java
+++ src/main/java/marketplace/dto/ProductRequestCreate.java
@@ -17,11 +17,11 @@ public class ProductRequestCreate {
     @NotNull(message = "The article cannot be empty")
     @Positive(message = "The article cannot be negative or zero")
     private Integer article;
-    @NotNull(message = "The name cannot be empty")
+    @NotBlank(message = "The name cannot be empty")
     private String name;
     private String description;
     private Category categories;
-    @NotNull(message = "The name cannot be empty")
+    @NotNull(message = "The price cannot be empty")
     @PositiveOrZero(message = "The price cannot be negative")
     private BigDecimal price;
     @PositiveOrZero(message = "The quantity cannot be negative")
