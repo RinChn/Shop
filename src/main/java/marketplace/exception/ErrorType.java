76310efdf15639af144c54437c4d03f6a9311970
RinChn <morik.arina@yandex.ru>
2025-01-14 21:48:00 +0200

    Adding errorType for files

--- src/main/java/marketplace/exception/ErrorType.java
+++ src/main/java/marketplace/exception/ErrorType.java
@@ -6,7 +6,9 @@ import lombok.Getter;
 public enum ErrorType {
     NONEXISTENT_ARTICLE("There is no product with this article."),
     DUPLICATE("Duplicate article."),
-    ERROR_FILE_SAVED("Failed to save workbook to file");
+    ERROR_FILE_SAVED("Failed to save workbook to file"),
+    INVALID_FILE_CONTENT("Couldn't convert file contents to save"),
+    NONEXISTEN_FILE("There is no report with this name."),;
 
     private final String message;
 
