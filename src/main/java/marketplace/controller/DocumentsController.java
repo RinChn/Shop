61211bcc08dfb1bddae85b2db837ce1816f76f97
RinChn <morik.arina@yandex.ru>
2025-01-14 21:47:41 +0200

    Adding the ability to upload and download a file

--- src/main/java/marketplace/controller/DocumentsController.java
+++ src/main/java/marketplace/controller/DocumentsController.java
@@ -3,11 +3,16 @@ package marketplace.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import marketplace.service.DocumentsServiceImpl;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import marketplace.util.FileHandler;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("api/v1/documents")
@@ -21,5 +26,22 @@ public class DocumentsController {
     public List<String> getReports() {
         log.info("Request to get the names of all the reporters");
         return documentService.allFilesNames();
+    }
+
+    @PostMapping("")
+    public String upload(@RequestParam("file") MultipartFile file) {
+        log.info("Uploading file {}", file.getOriginalFilename());
+        return documentService.uploadDocument(file);
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<ByteArrayResource> download(@PathVariable("name") String filename) {
+        log.info("Downloading file {}", filename);
+        MultipartFile file = documentService.downloadDocument(filename);
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +
+                        file.getOriginalFilename() + "\"")
+                .contentType(MediaType.parseMediaType(Objects.requireNonNull(file.getContentType())))
+                .body(new ByteArrayResource(FileHandler.getFileContent(file)));
     }
 }
--- src/main/java/marketplace/service/DocumentsServiceImpl.java
+++ src/main/java/marketplace/service/DocumentsServiceImpl.java
@@ -2,11 +2,20 @@ package marketplace.service;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import marketplace.exception.ApplicationException;
+import marketplace.exception.ErrorType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Service
 @Slf4j
@@ -22,5 +31,45 @@ public class DocumentsServiceImpl implements DocumentsService {
         }
         log.info("Returning all file names: {}", fileNames);
         return fileNames;
+    }
+
+    @Override
+    public String uploadDocument(MultipartFile multipartFile) {
+        Path path = Path.of("src/main/resources/reports",
+                Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        try {
+            multipartFile.transferTo(path);
+        } catch (IOException exception) {
+            throw new ApplicationException(ErrorType.INVALID_FILE_CONTENT);
+        }
+        return path.toString();
+    }
+
+    @Override
+    public MultipartFile downloadDocument(String fileName) {
+        File file = new File("src/main/resources/reports",
+                fileName);
+        FileInputStream input;
+        MultipartFile multipartFile;
+
+        if (!file.exists()) {
+            throw new ApplicationException(ErrorType.NONEXISTEN_FILE);
+        }
+        try {
+            input = new FileInputStream(file);
+        } catch (FileNotFoundException exception) {
+            throw new ApplicationException(ErrorType.NONEXISTEN_FILE);
+        }
+        try {
+            multipartFile = new MockMultipartFile(
+                    file.getName(),
+                    file.getName(),
+                    "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                    input
+            );
+        } catch (IOException exception) {
+            throw new ApplicationException(ErrorType.INVALID_FILE_CONTENT);
+        }
+        return multipartFile;
     }
 }
