adcbd2a7dad623c0251ef3054e1640816bec2f7c
RinChn <morik.arina@yandex.ru>
2025-01-25 22:35:59 +0300

    Adding work with multiple currencies to ExchangeRateHandler

--- src/main/java/marketplace/util/ExchangeRateHandler.java
+++ src/main/java/marketplace/util/ExchangeRateHandler.java
@@ -1,6 +1,7 @@
 package marketplace.util;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.servlet.http.HttpSession;
 import marketplace.exception.ApplicationException;
 import marketplace.exception.ErrorType;
 import lombok.extern.slf4j.Slf4j;
@@ -26,17 +27,23 @@ public class ExchangeRateHandler {
     @Value("${app.exchange.url}")
     private String exchangeServiceUrl;
 
-    public BigDecimal getUsdExchangeRateFromFile() {
-        ObjectMapper mapper = new ObjectMapper();
-        Map<String, Double> exchangeRate;
-        try {
-            exchangeRate = mapper.readValue(new File(filePath), Map.class);
-        } catch (IOException exception) {
-            log.error("Error reading file");
-            throw new ApplicationException(ErrorType.INVALID_EXCHANGE_RATE_FILE);
+    public String getCurrentCurrency(String currencyName, HttpSession session) {
+        if (currencyName != null && !currencyName.isEmpty()) {
+            currencyName = currencyName.toUpperCase();
+            Map<String, Double> allCurrencies = readAllCurrenciesFromFile();
+            for (String key : allCurrencies.keySet()) {
+                if (currencyName.equals(key)) {
+                    session.setAttribute("currency", currencyName);
+                    return currencyName;
+                }
+            }
         }
-        log.info("Successfully exchange rate from file");
-        return BigDecimal.valueOf(exchangeRate.get("USD"));
+        if (session.getAttribute("currency") != null)
+            return session.getAttribute("currency").toString();
+        else {
+            session.setAttribute("currency", "RUB");
+            return "RUB";
+        }
     }
 
     @Cacheable(value = "exchangeCache", key = "'usd'")
@@ -47,7 +54,34 @@ public class ExchangeRateHandler {
                     BigDecimal.class);
         } catch (RestClientException e) {
             log.warn("Failed to fetch USD exchange rate from service.");
-            return getUsdExchangeRateFromFile();
+            return getExchangeRateFromFile("USD");
+        }
+    }
+
+    @Cacheable(value = "exchangeCache", key = "'eur'")
+    public BigDecimal getEurFromService() {
+        try {
+            log.info("Successfully fetching EUR exchange rate from service");
+            return restTemplate.getForObject(exchangeServiceUrl + "/api/v2/exchange/eur",
+                    BigDecimal.class);
+        } catch (RestClientException e) {
+            log.warn("Failed to fetch EUR exchange rate from service.");
+            return getExchangeRateFromFile("EUR");
+        }
+    }
+
+    public BigDecimal getExchangeRateFromFile(String currencyName) {
+        Map<String, Double> exchangeRate = readAllCurrenciesFromFile();
+        log.info("Successfully exchange rate from file");
+        return BigDecimal.valueOf(exchangeRate.get(currencyName));
+    }
+
+    private Map<String, Double> readAllCurrenciesFromFile() {
+        try {
+            return new ObjectMapper().readValue(new File(filePath), Map.class);
+        } catch (IOException exception) {
+            log.error("Error reading file");
+            throw new ApplicationException(ErrorType.INVALID_EXCHANGE_RATE_FILE);
         }
     }
 }
