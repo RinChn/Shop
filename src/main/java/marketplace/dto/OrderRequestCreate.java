07c2efde5a07f2be1805bca802d090aa809acc20
RinChn <morik.arina@yandex.ru>
2025-01-29 01:10:21 +0300

    Rename email_customer → emailCustomer

--- src/main/java/marketplace/dto/OrderRequestCreate.java
+++ src/main/java/marketplace/dto/OrderRequestCreate.java
@@ -10,5 +10,5 @@ import lombok.*;
 @NoArgsConstructor
 public class OrderRequestCreate {
     @NotBlank(message = "The email cannot be empty")
-    String email_customer;
+    String emailCustomer;
 }
