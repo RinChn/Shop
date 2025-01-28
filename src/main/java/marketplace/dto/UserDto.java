221c45180abe97c66cd64a2f197be696c13fad90
RinChn <morik.arina@yandex.ru>
2025-01-29 01:10:56 +0300

    Adding annotations for UserDto

--- src/main/java/marketplace/dto/UserDto.java
+++ src/main/java/marketplace/dto/UserDto.java
@@ -1,7 +1,15 @@
 package marketplace.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
 public class UserDto {
     @NotBlank(message = "The email cannot be empty")
     String email;
