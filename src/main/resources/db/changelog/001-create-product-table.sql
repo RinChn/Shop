c41e50ecb55c6de6efb1a06b400bb26e3361f1a0
RinChn <morik.arina@yandex.ru>
2025-02-01 16:51:19 +0300

    Adding CHECK for quantity for product table

--- src/main/resources/db/changelog/001-create-product-table.sql
+++ src/main/resources/db/changelog/001-create-product-table.sql
@@ -9,7 +9,7 @@ CREATE TABLE products
     description TEXT,
     categories VARCHAR(255),
     price DECIMAL(38, 6) NOT NULL,
-    quantity INTEGER default 0,
+    quantity INTEGER default 0 CHECK (quantity >= 0),
     date_last_changes_quantity TIMESTAMP default CURRENT_TIMESTAMP,
     date_creation TIMESTAMP default CURRENT_TIMESTAMP,
     PRIMARY KEY (id)
