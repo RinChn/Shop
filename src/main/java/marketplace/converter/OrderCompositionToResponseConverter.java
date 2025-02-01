7414a9596217ec218cf64b52f03d796921caaac0
RinChn <morik.arina@yandex.ru>
2025-02-01 16:57:26 +0300

    Adding new converters

---
+++ src/main/java/marketplace/converter/OrderCompositionToResponseConverter.java
@@ -0,0 +1,19 @@
+package marketplace.converter;
+
+import marketplace.dto.OrderCompositionResponse;
+import marketplace.entity.OrderComposition;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class OrderCompositionToResponseConverter implements Converter<OrderComposition, OrderCompositionResponse> {
+
+    @Override
+    public OrderCompositionResponse convert(OrderComposition source) {
+        return OrderCompositionResponse.builder()
+                .productArticle(source.getProduct().getArticle())
+                .productQuantity(source.getProductQuantity())
+                .productPrice(source.getPrice())
+                .build();
+    }
+}
