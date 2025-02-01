6fc95a638958885a265eeb6ca334b4ebda882526
RinChn <morik.arina@yandex.ru>
2025-02-01 16:51:44 +0300

    Adding default value for quantity for order_composition table

--- src/main/resources/db/changelog/005-create-order-compositions-table.sql
+++ src/main/resources/db/changelog/005-create-order-compositions-table.sql
@@ -5,7 +5,7 @@ CREATE TABLE order_compositions
 (
     product_id UUID REFERENCES products(id),
     order_id UUID REFERENCES orders(id),
-    product_quantity INTEGER,
+    product_quantity INTEGER default 1,
     price DECIMAL(38, 6),
     primary key (order_id, product_id)
 )
