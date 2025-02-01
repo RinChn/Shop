0dbb9e50a48b1a67b53911f38ee5c49b1cfd31d5
RinChn <morik.arina@yandex.ru>
2025-02-01 16:53:13 +0300

    Adding fields for price and q

--- src/main/java/marketplace/entity/OrderComposition.java
+++ src/main/java/marketplace/entity/OrderComposition.java
@@ -3,6 +3,8 @@ package marketplace.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
+
 @Entity
 @Table(name = "order_compositions")
 @Getter
@@ -21,5 +23,9 @@ public class OrderComposition {
     @MapsId("productId")
     @JoinColumn(name = "product_id", nullable = false)
     Product product;
-
+    @Column(name = "product_quantity", columnDefinition = "integer default 1")
+            @Builder.Default
+    Integer productQuantity = 1;
+    @Column(name = "price")
+    BigDecimal price;
 }
