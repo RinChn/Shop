1245e25777bc890614562cea1568979105c90b6a
RinChn <morik.arina@yandex.ru>
2025-02-02 23:32:44 +0300

    Adding @Component in UserToResponseConverter

--- src/main/java/marketplace/converter/UserToResponseConverter.java
+++ src/main/java/marketplace/converter/UserToResponseConverter.java
@@ -3,7 +3,9 @@ package marketplace.converter;
 import marketplace.dto.UserDto;
 import marketplace.entity.User;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserToResponseConverter implements Converter<User, UserDto> {
     @Override
     public UserDto convert(User source) {
