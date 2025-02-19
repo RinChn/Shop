ef07fdaedda460438af6777c4782e6a74f2e26a2
RinChn <morik.arina@yandex.ru>
2025-02-19 23:26:19 +0300

    Creating a new error type for incorrect events

--- src/main/java/marketplace/exception/ErrorType.java
+++ src/main/java/marketplace/exception/ErrorType.java
@@ -18,7 +18,8 @@ public enum ErrorType {
     ERROR_FILE_SAVED("Failed to save workbook to file"),
     INVALID_FILE_CONTENT("Couldn't convert file contents to save"),
     NONEXISTEN_FILE("There is no report with this name"),
-    INVALID_EXCHANGE_RATE_FILE("Invalid exchange rate file");
+    INVALID_EXCHANGE_RATE_FILE("Invalid exchange rate file"),
+    INCORRECT_EVENT_NAME("Received a non-existent event name");
 
     private final String message;
 
